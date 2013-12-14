ad2ffeb52b7c3cba84b6e52810b2017bdcbe64c3
Mark Moissette <kaosat.dev@gmail.com>
2013-12-14 10:01:03 +0100

    - cleanup / bugfix of object3D boundingSphere computation system

--- usco-ultiviewer/lib/computeBounding.js
+++ usco-ultiviewer/lib/computeBounding.js
@@ -7,14 +7,24 @@ computeObject3DBoundingSphere = function(object)
   }
   else
   {
+    
+    if (! object.geometry.boundingBox )
+    {
+      object.geometry.computeBoundingBox();
+    } 
     var bbox = object.geometry.boundingBox.clone();
   }
   
   object.traverse(function (child) {
+
     if (child instanceof THREE.Mesh)
     {
         if( child.geometry !==undefined)
         {
+             
+          if (! child.geometry.boundingBox ){
+            child.geometry.computeBoundingBox();
+          }
           var childBox = child.geometry.boundingBox.clone();
           childBox.translate( child.localToWorld( new THREE.Vector3() ) );
           bbox.union( childBox );
