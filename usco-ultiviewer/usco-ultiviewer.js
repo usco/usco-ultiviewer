fe640b0ac58b196dafcd80e2587efc8ea7cf0296
Mark Moissette <kaosat.dev@gmail.com>
2014-01-29 14:18:43 +0100

    - further enhancements to resource loader on screen display:  * moved resource size warning from viewer to resource-loader element  * enhanced css & overal structure
    - related cleanup to usco-ultiviewer

--- usco-ultiviewer/usco-ultiviewer.js
+++ usco-ultiviewer/usco-ultiviewer.js
@@ -30,7 +30,6 @@ Polymer('usco-ultiviewer', {
     this._assetManager.stores["xhr"].timeout = 0;
     
   
-    this.warningSize = 100000;//byte size above which to display a warning to the user
     this.minObjectSize = 40;//minimum size (in arbitrarty/opengl units) before requiring re-scaling (upwards)
     this.maxObjectSize = 100;//maximum size (in arbitrarty/opengl units) before requiring re-scaling (downwards)
 
